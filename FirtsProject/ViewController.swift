a81e85941b781fde2df6a4816f1fe7b94df811fe
Viktar Charniak <moodooosgrodno@gmail.com>
2024-04-23 17:46:56 -0400

    Initial Commit

---
+++ FirtsProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FirtsProject
+//
+//  Created by Viktar Charniak on 4/23/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
